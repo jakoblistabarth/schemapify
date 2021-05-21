1536d5b4a397c957c4e40d746c645c1dc69b4c8a
Jakob Listabarth <jakob@listabarth.at>
2021-05-21 23:28:13 +0200

    add test data, containing edge cases

--- assets/main.js
+++ assets/main.js
@@ -2,16 +2,16 @@ import DCEL from "./lib/dcel/Dcel.mjs";
 import { logDCEL, mapFromDCEL } from "./lib/dcel/Utilities.mjs";
 
 const tests = [
-  "assets/data/geodata/ne_110m_africa_admin0.json",
+  // "assets/data/geodata/ne_110m_africa_admin0.json",
   // "assets/data/geodata/AUT_adm1.json",
   // "assets/data/geodata/central-austria.json",
   // "assets/data/shapes/triangle.json",
-  "assets/data/shapes/enclave.json",
+  // "assets/data/shapes/enclave.json",
   // "assets/data/shapes/enclave2.json",
   // "assets/data/shapes/triangle-hole.json",
   // "assets/data/shapes/2triangle-adjacent.json",
   // "assets/data/shapes/square.json",
-  "assets/data/shapes/square-islands.json",
+  // "assets/data/shapes/square-islands.json",
   // "assets/data/shapes/square-hole.json",
   // "assets/data/shapes/square-hole-island.json",
   // "assets/data/shapes/square-hole-island-hole.json",
@@ -19,9 +19,10 @@ const tests = [
   // "assets/data/shapes/2plgn-adjacent.json",
   // "assets/data/shapes/2plgn-islands.json",
   // "assets/data/shapes/2plgn-islands-hole.json",
-  "assets/data/shapes/2plgn-islands-holes.json",
+  // "assets/data/shapes/2plgn-islands-holes.json",
   // "assets/data/shapes/3plgn.json",
   // "assets/data/shapes/3plgn-complex.json",
+  "assets/data/shapes/edge-cases.json",
 ];
 
 async function getJSON(path) {
@@ -32,12 +33,7 @@ async function getJSON(path) {
 tests.forEach(async (test) => {
   const data = await getJSON(test);
   const name = test.slice(test.lastIndexOf("/") + 1, -5);
-
   const subdivision = DCEL.buildFromGeoJSON(data);
-
-  // logDCEL(subdivision, name);
-  // mapFromDCEL(subdivision, name);
-
   subdivision.splitEdges();
 
   logDCEL(subdivision, name);
