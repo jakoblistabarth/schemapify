23028dd152a4762773973fae54fc2722b21b7d0b
Jakob Listabarth <jakob@listabarth.at>
2021-06-10 18:26:31 +0200

    compute staircase and its region for aligned deviating edges

--- assets/lib/Staircase.mjs
+++ assets/lib/Staircase.mjs
@@ -1,24 +1,22 @@
 import { EDGE_CLASSES } from "./dcel/HalfEdge.mjs";
 import Point from "./Point.mjs";
 import Line from "./Line.mjs";
+import ConvexHullGrahamScan from "graham_scan";
 
 class Staircase {
-  constructor(halfEdge) {
-    this.halfEdge = halfEdge;
-    this.region = this.getRegion(halfEdge);
-    this.de = null; // TODO: move this to HalfEdge? not really related to halfEdge
-    this.stepNumbers = null;
-    this.geometry = null; // contains edges and vertices constructing the staircase
+  constructor(edge) {
+    this.edge = edge;
+    this.region = this.getRegion();
+    this.de; // TODO: move this to HalfEdge? not really related to edge
+    this.stepNumbers;
+    this.stepPoints;
   }
 
   getRegion() {
-    const edge = this.halfEdge;
+    const edge = this.edge;
     const edgeClass = edge.class;
     if (edgeClass === EDGE_CLASSES.AB) {
-      return [
-        [edge.getTail().x, edge.getTail().y],
-        [edge.getHead().x, edge.geHead().y],
-      ];
+      return [edge.getTail().xy(), edge.getHead().xy()]; // QUESTION: better 4 coordinates to span an area?
     } else if (edgeClass === EDGE_CLASSES.UB || edgeClass === EDGE_CLASSES.E) {
       const [lower, upper] = edge.getAssociatedSector()[0].getBounds();
       const A = new Point(...edge.getTail().xy());
@@ -31,12 +29,13 @@ class Staircase {
       const D = d.intersectsLine(c);
       return [A, B, C, D];
     } else if (edgeClass === EDGE_CLASSES.UD) {
-      // TODO: like ub and e but accommodate for the appendex area
+      // TODO: like UB and E but accommodate for the appendex area
       return;
     } else if (edgeClass === EDGE_CLASSES.AD) {
-      // TODO: implement bounding box
-      const _deltaE = edge.getLength() * 0.1;
-      return;
+      this.stepPoints = this.getStaircasePoints(this.edge);
+      const convexHull = new ConvexHullGrahamScan();
+      this.stepPoints.forEach((p) => convexHull.addPoint(p.x, p.y));
+      return convexHull.getHull();
     }
   }
 
@@ -54,17 +53,35 @@ class Staircase {
     return stepNumbers;
   }
 
-  buildStaircase() {
-    const edge = this.halfEdge;
-    const edgeClass = edge.class;
-    if (edgeClass === EDGE_CLASSES.AD) {
-      const epsilon = 0.1;
-      d1 = edge.class;
-      d2 = edge.getAngle();
+  getStaircasePoints(edge) {
+    switch (edge.class) {
+      case EDGE_CLASSES.AD:
+        return this.getStairCasePointsAD(edge);
+      case EDGE_CLASSES.A:
+        return; // TODO: implement other cases
     }
+  }
 
-    this.geometry = staircase;
-    return staircase;
+  getStairCasePointsAD(edge) {
+    const epsilon = 0.1;
+    const deltaE = edge.getLength() * epsilon;
+    const d1 = this.edge.dcel.config.C.getAngles()[edge.assignedAngle];
+    const d2 = edge.getAngle();
+    const d1Opposite = (d1 + Math.PI) % (Math.PI * 2);
+    // console.log("d1", d1, "d2", d2, "deltaE", deltaE);
+
+    const points = [];
+
+    points[0] = edge.getTail();
+    points[6] = edge.getHead();
+
+    points[1] = points[0].getNewPoint(deltaE, d1);
+    points[2] = points[1].getNewPoint((edge.getLength() * (1 - epsilon)) / 2, d2);
+    points[3] = points[2].getNewPoint(deltaE * 2, d1Opposite);
+    points[4] = points[3].getNewPoint((edge.getLength() * (1 - epsilon)) / 2, d2);
+    points[5] = points[4].getNewPoint(deltaE, d1);
+
+    return points;
   }
 }
 
--- spec/StaircaseSpec.mjs
+++ spec/StaircaseSpec.mjs
@@ -56,10 +56,31 @@ describe("The Staircase class", function () {
 
     const staircase = new Staircase(edge);
     expect(staircase.region).toEqual([
-      [0, 0],
-      [0, 2],
-      [-10, 2],
-      [-10, 0],
+      new Point(0, 0),
+      new Point(0, 2),
+      new Point(-10, 2),
+      new Point(-10, 0),
     ]);
+  });
+});
+
+describe("buildStaircaseAD", function () {
+  it("returns a Staircase for an unaligned deviating halfEdge with 7 Vertices", function () {
+    const dcel = new Dcel();
+    dcel.config = { C: new C(4) };
+
+    const o = new Vertex(0, 0);
+    const d = new Vertex(10, 10);
+    const edge = dcel.makeHalfEdge(o, d);
+    const twin = dcel.makeHalfEdge(d, o);
+    edge.twin = twin;
+    twin.twin = edge;
+    edge.class = EDGE_CLASSES.AD;
+    edge.assignedAngle = 0;
+    edge.dcel = dcel;
+
+    const staircase = new Staircase(edge);
+    expect(staircase.stepPoints.length).toBe(7);
+    expect(staircase.region.length).toBeLessThanOrEqual(staircase.stepPoints.length);
   });
 });
