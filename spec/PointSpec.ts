f6157c9ba2f89b507bd9cb3200805d84d5ee2867
Jakob Listabarth <jakob@listabarth.at>
2021-07-06 15:32:26 +0200

    fix isInPolygon specs

--- spec/PointSpec.ts
+++ spec/PointSpec.ts
@@ -30,16 +30,18 @@ describe("isInPolygon()", function () {
     expect(c.isInPolygon(A)).toBeFalse();
   });
 
-  it("returns false if it lies inside of a convex(!) Polygon", function () {
+  it("returns true if it lies inside of a convex(!) Polygon", function () {
     const A = [new Point(0, 0), new Point(0, 1), new Point(1, 1), new Point(1, 0)];
     const B = [new Point(-5, -5), new Point(5, -5), new Point(5, 10), new Point(-5, 10)];
     const a = new Point(0.5, 0.5);
     const b = new Point(0, 1);
-    const c = new Point(-2.5, -2.5);
+    const c = new Point(0.5, 0);
+    const d = new Point(-2.5, -2.5);
 
     expect(a.isInPolygon(A)).toBeTrue();
-    expect(a.isInPolygon(A)).toBeTrue();
-    expect(c.isInPolygon(B)).toBeTrue();
+    expect(b.isInPolygon(A)).toBeTrue();
+    expect(c.isInPolygon(A)).toBeTrue();
+    expect(d.isInPolygon(B)).toBeTrue();
   });
 });
 
