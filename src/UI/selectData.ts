f74ec611f8e4e3bc37eec22c1625ef2c5fae2705
Jakob Listabarth <jakob@listabarth.at>
2021-07-15 14:14:56 +0200

    Add shape with AD edges to test options

--- src/UI/selectData.ts
+++ src/UI/selectData.ts
@@ -59,6 +59,6 @@ export const tests: string[] = [
   "shapes/triangle-unaligned.json",
   // "shapes/triangle.json",
   "shapes/unaligned-deviating-2.json",
-  // "shapes/unaligned-deviating.json",
+  "shapes/unaligned-deviating.json",
   "shapes/v-shape.json",
 ];
