74892dfdf9f9479e0139dfa3132d8b1567870229
Jakob Listabarth <76778035+jakoblistabarth@users.noreply.github.com>
2023-05-18 18:20:11 +0200

    :adhesive_bandage: Prevent dragging if no data is loaded

--- app/components/Map/Map.tsx
+++ app/components/Map/Map.tsx
@@ -1,14 +1,18 @@
+import useAppStore from "@/app/helpers/store";
 import "leaflet/dist/leaflet.css";
 import { FC, PropsWithChildren } from "react";
 import { MapContainer } from "react-leaflet";
 
 const Map: FC<PropsWithChildren> = ({ children, ...rest }) => {
+  const { source } = useAppStore();
   return (
     <MapContainer
       zoom={0}
       style={{ height: "100%" }}
       zoomControl={false}
       doubleClickZoom={false}
+      dragging={source ? true : false}
+      key={source?.name ?? ""}
       {...rest}
     >
       {children}
