4545e1d26163adab361a26b0739258cbfd597494
Jakob Listabarth <76778035+jakoblistabarth@users.noreply.github.com>
2024-03-18 12:50:25 +0100

    :technologist: Use ESLint (#18)
    * Add ESLint config
    * Add ESLint to gh action

--- app/components/Brand.tsx
+++ app/components/Brand.tsx
@@ -1,7 +1,10 @@
+import Image from "next/image";
+import brand from "../../public/schemapify-mark.svg";
+
 const Brand = () => {
   return (
     <div className="relative z-above-map flex items-center">
-      <img src={"/schemapify-mark.svg"} className="mr-2 h-6" />
+      <Image alt="schemapify logo" src={brand} className="mr-2 w-6" />
       <h1 className="font-display text-3xl">Schemapify</h1>
     </div>
   );
