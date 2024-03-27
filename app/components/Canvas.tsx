96287e41cd661bbf35336613646d2b31d992e8a6
Jakob Listabarth <76778035+jakoblistabarth@users.noreply.github.com>
2024-03-27 11:58:17 +0100

    :sparkles: Center canvas on geometry

--- app/components/Canvas.tsx
+++ app/components/Canvas.tsx
@@ -5,13 +5,12 @@ import { OrthographicView } from "@deck.gl/core/typed";
 import { LineLayer, ScatterplotLayer } from "@deck.gl/layers/typed";
 import { DeckGL } from "@deck.gl/react/typed";
 import { FC } from "react";
-import geometry from "@/test/data/shapes/unaligned-deviating-2.json";
-import { FeatureCollection } from "geojson";
 import HalfEdge from "@/src/DCEL/HalfEdge";
 import { range } from "d3";
+import { TwoPlgnIslandsHoles } from "@/test/data/shapes/2plg-islands-holes";
 
 const Canvas: FC = () => {
-  const dcel = Dcel.fromGeoJSON(geometry as FeatureCollection);
+  const dcel = Dcel.fromMultiPolygons(TwoPlgnIslandsHoles);
   dcel.schematize();
 
   const vertices = Array.from(dcel.getVertices());
@@ -30,25 +29,32 @@ const Canvas: FC = () => {
     stroked: true,
     lineWidthUnits: "pixels",
     lineWidthMinPixels: 1,
-    getLineColor: [0,0,0],
-    getFillColor: [255,255,255],
+    getLineColor: [0, 0, 0],
+    getFillColor: [255, 255, 255],
   });
 
-  const gridPoints = range(-50, 50, .5).map((i) => range(-50, 50, .5).map((j) => [i,j])).flat()
+  const gridPoints = range(-50, 50, 0.5)
+    .map((i) => range(-50, 50, 0.5).map((j) => [i, j]))
+    .flat();
 
   const grid = new ScatterplotLayer({
     data: gridPoints,
     getPosition: (d) => d,
     radiusMinPixels: 1,
     radiusMaxPixels: 1,
-    getFillColor: [0,0,0,50],
+    getFillColor: [0, 0, 0, 50],
   });
 
   return (
     <DeckGL
       views={new OrthographicView({ flipY: false })}
       layers={[grid, edges, points]}
-      initialViewState={{ target: [0, 0], zoom: 4, minZoom: 4, maxZoom: 7, }}
+      initialViewState={{
+        target: dcel.center,
+        zoom: 6,
+        minZoom: 5,
+        maxZoom: 7,
+      }}
       controller={true}
     />
   );
