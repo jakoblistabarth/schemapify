62ddfee5f67a10e14dfd69448bad8ba090d12a01
Jakob Listabarth <76778035+jakoblistabarth@users.noreply.github.com>
2024-03-27 11:05:34 +0100

    :recycle: Make Ring return points counterclockwise

--- test/spec/Ring.spec.ts
+++ test/spec/Ring.spec.ts
@@ -2,16 +2,24 @@ import Ring from "@/src/geometry/Ring";
 
 describe("isClockwise() returns the correct boolean", function () {
   it("for simple squares", function () {
-    const points: [number, number][] = [
+    const counterclockwise: [number, number][] = [
       [0, 0],
       [1, 0],
       [1, 1],
       [0, 1],
     ];
 
-    expect(Ring.fromCoordinates(points).isClockwise).toBeFalsy();
+    const clockwise: [number, number][] = [
+      [0, 0],
+      [0, 1],
+      [1, 1],
+      [1, 0],
+    ];
+
+    expect(Ring.fromCoordinates(clockwise).isClockwise).toBeTruthy();
+    expect(Ring.fromCoordinates(counterclockwise).isClockwise).toBeFalsy();
     expect(
-      Ring.fromCoordinates([...points].reverse()).isClockwise,
+      Ring.fromCoordinates([...counterclockwise].reverse()).isClockwise,
     ).toBeTruthy();
   });
 
@@ -66,40 +74,5 @@ describe("isClockwise() returns the correct boolean", function () {
     expect(
       Ring.fromCoordinates([...points].reverse()).isClockwise,
     ).toBeTruthy();
-  });
-});
-
-describe("reverse() revers the order of a", function () {
-  it("counterclockwise ring", function () {
-    const r = Ring.fromCoordinates([
-      [0, 0],
-      [1, 0],
-      [1, 1],
-      [0, 1],
-    ]);
-
-    expect(r.reverse().isClockwise).toBeTruthy();
-  });
-  it("clockwise ring", function () {
-    const r = Ring.fromCoordinates([
-      [0, 0],
-      [0, 1],
-      [1, 1],
-      [1, 0],
-    ]);
-
-    expect(r.reverse().isClockwise).toBeFalsy();
-  });
-
-  it("a concave ring", function () {
-    const r = Ring.fromCoordinates([
-      [0, 0],
-      [1, 0],
-      [1, 1],
-      [0.5, 0.5],
-      [0, 1],
-    ]);
-
-    expect(r.reverse().isClockwise).toBeTruthy();
   });
 });
