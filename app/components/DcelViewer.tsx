dfb3f56add090100393644cd3cea1b65e3718260
Jakob Listabarth <jakob@listabarth.at>
2024-04-11 08:32:22 +0200

    🚧 Add toolbar to DcelViewer

--- app/components/DcelViewer.tsx
+++ app/components/DcelViewer.tsx
@@ -3,7 +3,7 @@
 import Dcel from "@/src/DCEL/Dcel";
 import MultiPolygon from "@/src/geometry/MultiPolygon";
 import { FC, useMemo, useState } from "react";
-import { RxPause, RxResume } from "react-icons/rx";
+import { RxLayers, RxPause, RxResume } from "react-icons/rx";
 import Button from "./Button";
 import Canvas from "./Canvas";
 
@@ -28,9 +28,12 @@ const DcelViewer: FC = () => {
   }, []);
   return (
     <div>
-      <div className="my-2">
+      <div className="my-2 flex justify-between gap-2 p-2 shadow">
         <Button onClick={() => setIsAnimating(!isAnimating)}>
           {isAnimating ? <RxPause /> : <RxResume />}
+        </Button>
+        <Button>
+          <RxLayers />
         </Button>
       </div>
       <div className="relative min-h-[500px] overflow-hidden rounded bg-gray-200/25">
