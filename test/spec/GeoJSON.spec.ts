9b87b1d7ffa03cd1f7d8640f5037ac521786b00a
Jakob Listabarth <76778035+jakoblistabarth@users.noreply.github.com>
2025-07-28 00:16:56 +0200

    :recycle: Validate wrong geometry type in separate test
    Linestrings.json contains valid geometries, but the geometry type LineString is not suitable for the polygon schematization tool

--- test/spec/GeoJSON.spec.ts
+++ test/spec/GeoJSON.spec.ts
@@ -2,13 +2,14 @@ import fs from "fs";
 import path from "path";
 import { getTestFiles } from "./test-setup";
 import { hint } from "@mapbox/geojsonhint";
+import Dcel from "@/src/Dcel/Dcel";
 
 describe("validate geoJSON file (simple shape)", function () {
   const dir = "test/data/shapes";
   const testFiles = getTestFiles(dir);
 
   testFiles.forEach((file) => {
-    xit(file + " to return 0 errors, e.i., to be valid", function () {
+    it(file + " to return 0 errors, e.i., to be valid", function () {
       const json = JSON.parse(
         fs.readFileSync(path.resolve(dir + "/" + file), "utf8"),
       );
@@ -33,17 +34,31 @@ describe("validate geoJSON file (geodata)", function () {
   });
 });
 
+// linestrings.json is actually not invalid, by the geojson specification, but it is not supported by the schematization tool (targeting polygons)
 describe("Find errors for invalid geoJSON file (own example)", function () {
   const dir = "test/data/invalid";
-  const testFiles = getTestFiles(dir);
+  const testFiles = getTestFiles(dir).filter((f) => f !== "linestrings.json");
 
   testFiles.forEach((file) => {
-    xit(file, function () {
+    it(file, function () {
       const json = JSON.parse(
         fs.readFileSync(path.resolve(dir + "/" + file), "utf8"),
       );
       const errors = hint(JSON.stringify(json, null, 4));
       expect(errors.length).toBeGreaterThan(0);
     });
+  });
+});
+
+describe("If the input data is not a region i.e., it contains features of type other than polygon or multipolygon – the program shall exit and the user shall be informed.", function () {
+  it("An error is thrown for a file containing geometry of type 'LineString'.", function () {
+    const json = JSON.parse(
+      fs.readFileSync(
+        path.resolve("test/data/invalid/linestrings.json"),
+        "utf8",
+      ),
+    );
+    console.log({ json });
+    expect(() => Dcel.fromGeoJSON(json)).toThrow("invalid input");
   });
 });
