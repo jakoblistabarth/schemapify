92ac0d8efef4d37a25b3c77d762be5e4538b0190
Jakob Listabarth <76778035+jakoblistabarth@users.noreply.github.com>
2026-02-11 16:30:54 +0100

    :white_check_mark: Validate some invalid geometries with turf
    As turf's booleanValid implementation follows the OGC Simple Feature Specification and not specifically the GeoJSON specification this test case is discarded for now

--- test/spec/GeoJSON.spec.ts
+++ test/spec/GeoJSON.spec.ts
@@ -3,6 +3,8 @@ import path from "path";
 import { getTestFiles } from "./test-setup";
 import { scavenge } from "@placemarkio/check-geojson";
 import Dcel from "@/src/Dcel/Dcel";
+import { featureReduce } from "@turf/meta";
+import booleanValid from "@turf/boolean-valid";
 
 describe("validate geoJSON file (simple shape)", function () {
   const dir = "test/data/shapes";
@@ -36,33 +38,10 @@ describe("validate geoJSON file (geodata)", function () {
 });
 
 describe("Find errors for invalid geoJSON file", function () {
-  // @placemarkio/check-geojson does not (yet) support (all?) cases with invalid geometry
-  xit("with a unclosed polygon", function () {
+  it("with a unclosed polygon", function () {
     const json = JSON.parse(
       fs.readFileSync(
-        path.resolve("test/data/invalid/square-not-closed.json"),
-        "utf8",
-      ),
-    );
-    const { rejected } = scavenge(JSON.stringify(json, null, 4));
-    expect(rejected.length).toBeGreaterThan(0);
-  });
-
-  xit("with a polygon in wrong winding-order", function () {
-    const json = JSON.parse(
-      fs.readFileSync(
-        path.resolve("test/data/invalid/square-right-hand-rule-violation.json"),
-        "utf8",
-      ),
-    );
-    const { rejected } = scavenge(JSON.stringify(json, null, 4));
-    expect(rejected.length).toBeGreaterThan(0);
-  });
-
-  xit("with a polygon containing a loop edge", function () {
-    const json = JSON.parse(
-      fs.readFileSync(
-        path.resolve("test/data/invalid/square-loop-edge.json"),
+        path.resolve("test/data/invalid/not-closed.json"),
         "utf8",
       ),
     );
@@ -81,6 +60,22 @@ describe("Find errors for invalid geoJSON file", function () {
     );
     const { rejected } = scavenge(JSON.stringify(json, null, 4));
     expect(rejected.length).toBeGreaterThan(0);
+  });
+
+  // use turf for validation with advanced geometry-checks
+  it("with a polygon containing a loop edge", function () {
+    const json = JSON.parse(
+      fs.readFileSync(
+        path.resolve("test/data/invalid/square-loop-edge.json"),
+        "utf8",
+      ),
+    );
+    const isValid = featureReduce(
+      json,
+      (_, feature) => (booleanValid(feature) ? true : false),
+      true,
+    );
+    expect(isValid).toBe(false);
   });
 });
 
