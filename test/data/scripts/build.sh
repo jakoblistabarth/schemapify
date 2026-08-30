9079f16958923ed350c8717cff49a4bb69d76483
Jakob Listabarth <76778035+jakoblistabarth@users.noreply.github.com>
2026-08-30 21:14:14 +0200

    :white_check_mark: Crreate simplified variants of testdata
    Use mapshaper to create topologically-aware simplified test data for manual testing

--- test/data/scripts/build.sh
+++ test/data/scripts/build.sh
@@ -9,3 +9,14 @@ for file in test/data/scripts/*.sql; do
   echo "→ $file"
   duckdb -f "$file"
 done
+
+# Create simplified (topology aware) variants
+# with mapshaper to test without the full-resolution cost
+for percent in 0.01 .05 .1; do
+  out="test/data/generated/DEU_adm1-s${percent}.gpkg"
+  echo "→ $out"
+  pnpm exec mapshaper test/data/generated/DEU_adm1.gpkg \
+    -simplify "${percent}%" keep-shapes \
+    -clean \
+    -o "$out"
+done
