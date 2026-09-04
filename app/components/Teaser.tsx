a4a187381878c4064cc13878af3239f53154b391
Jakob Listabarth <76778035+jakoblistabarth@users.noreply.github.com>
2026-09-04 23:43:06 +0200

    :speech_balloon: Make wording consistent

--- app/components/Teaser.tsx
+++ app/components/Teaser.tsx
@@ -8,7 +8,7 @@ const Teaser = () => {
   return (
     <div className="direction font-display relative h-full max-w-2xl flex-row items-center">
       <h2 className="text-3xl font-black font-stretch-ultra-condensed sm:text-4xl md:text-6xl">
-        Schematic maps<span className="text-blue-500">*</span> on&nbsp;demand.
+        Schematized maps<span className="text-blue-500">*</span> on&nbsp;demand.
       </h2>
       <p className="mt-10 text-xl">Shave down vertices for smoother maps.</p>
       <p className="font-sans text-sm">
