6d1b285275e03683a2dd9010aefe776ab354c6f4
Jakob Listabarth <76778035+jakoblistabarth@users.noreply.github.com>
2026-09-04 19:45:03 +0200

    :speech_balloon: Update teaser text

--- app/components/Teaser.tsx
+++ app/components/Teaser.tsx
@@ -6,12 +6,13 @@ const Teaser = () => {
   const { source } = useAppStore();
   if (source) return <></>;
   return (
-    <div className="direction font-display relative h-full flex-row items-center pr-[30%]">
-      <h2 className="text-3xl font-black sm:text-4xl md:text-6xl">
-        Schematic maps* on demand.
+    <div className="direction font-display relative h-full max-w-2xl flex-row items-center">
+      <h2 className="text-3xl font-black font-stretch-ultra-condensed sm:text-4xl md:text-6xl">
+        Schematic maps<span className="text-blue-500">*</span> on&nbsp;demand.
       </h2>
-      <p className="mt-5">
-        * Maps of regions though. Sorry, no network-based transit maps here. 😐
+      <p className="mt-10 text-xl">Shave down vertices for smoother maps.</p>
+      <p className="font-sans text-sm">
+        <span className="text-blue-500">*</span> Polygons only.
       </p>
     </div>
   );
